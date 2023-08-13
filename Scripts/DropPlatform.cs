54ecf94a8cbb834edb482faa4d1658701f899bac
ManavGhaiCode <manavghai12october@gmail.com>
2023-08-13 15:16:09 +0530

    add OneWayPlatforms

--- Scripts/DropPlatform.cs
+++ Scripts/DropPlatform.cs
@@ -1,18 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class DropPlatform : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class DropPlatform : MonoBehaviour {
+    private PlatformEffector2D Effector;
+    private float waitTime = .15f;
+
+    void Start() {
+        Effector = GetComponent<PlatformEffector2D>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        if (Input.GetKey("s")) {
+            if (waitTime <= 0) {
+                Effector.rotationalOffset = 180f;
+            } else {
+                waitTime -= Time.deltaTime;
+            }
+        } else {
+            waitTime = .15f;
+            Effector.rotationalOffset = 0f;
+        }
     }
 }
